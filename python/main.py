970ab87934a40e68dd1be8a111e1a2ed6635e85c
Edgar <86738291+OGShawnLee@users.noreply.github.com>
2023-09-20 20:44:16 +0000

    feat(py): add second task solution

--- python/main.py
+++ python/main.py
@@ -14,5 +14,23 @@ def run_first_task():
     utils.report_value("Crime", crime)
 
 
+def run_second_task():
+    a = utils.get_float("Enter a number for a")
+    b = utils.get_float("Enter a number for b")
+    c = utils.get_float("Enter a number for c")
+    d = utils.get_float("Enter a number for d")
+    e = utils.get_float("Enter a number for e")
+    f = utils.get_float("Enter a number for f")
+
+    result = (a + b ) / (c + d)    
+    utils.report_value("Result of (a + b) / (c + d)", result)
+
+    result = (a + (b / c)) / (d + (e / f))
+    utils.report_value("Result of (a + (b / c)) / (d + (e / f))", result)
+
+    result = a + (b / (c - d))
+    utils.report_value("Result of a (b / (c - d))", result)
+
 if __name__ == "__main__":
-    run_first_task()
+    # run_first_task()
+    run_second_task()
