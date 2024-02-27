08a75f049675514db1261b303a4fca6151d9a612
Shawn <changoloco.evg@gmail.com>
2024-02-27 20:49:11 +0000

    feat(cpp/ds): add 2_fn_recursion classwork

---
+++ c/data-structures/2_fn_recursion.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int get_int_arr_sum(int int_arr[], size_t len) {
+  int count = int_arr[0];
+  for (size_t i = 1; i < len; i++) {
+    count += int_arr[i];
+  }
+  return count;
+}
+
+int get_int_arr_sum_rec(int arr[], size_t len) {
+  if (len == 0) return 0;
+
+  return arr[len - 1] + get_int_arr_sum_rec(arr, len - 1);
+}
+
+int main() {
+  size_t len = 5;
+  int int_arr[] = { 1, 2, 3, 4, 5 };
+  int count = get_int_arr_sum(int_arr, len);
+  printf("Sum of int_arr is: %d\n", count);
+
+  count = get_int_arr_sum_rec(int_arr, len);
+  printf("Sum of int_arr recursive is: %d\n", count);
+
+  return 0;
+}
