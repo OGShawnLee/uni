d4a322c268430d039fd5cb52b9d8a90862029a00
Shawn <changoloco.evg@gmail.com>
2024-02-28 20:04:48 +0000

    feat(cpp/ds): add another case

--- c/data-structures/2_fn_recursion.cpp
+++ c/data-structures/2_fn_recursion.cpp
@@ -13,7 +13,7 @@ int get_int_arr_sum_rec(int arr[], size_t len) {
 
   return arr[len - 1] + get_int_arr_sum_rec(arr, len - 1);
 }
-
+ 
 int main() {
   size_t len = 5;
   int int_arr[] = { 1, 2, 3, 4, 5 };
@@ -21,6 +21,14 @@ int main() {
   printf("Sum of int_arr is: %d\n", count);
 
   count = get_int_arr_sum_rec(int_arr, len);
+  printf("Sum of int_arr recursive is: %d\n", count);
+
+  int int_arr_b[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+  count = get_int_arr_sum(int_arr_b, 10);
+  printf("Sum of int_arr is: %d\n", count);
+
+  count = get_int_arr_sum_rec(int_arr_b, 10);
   printf("Sum of int_arr recursive is: %d\n", count);
 
   return 0;
